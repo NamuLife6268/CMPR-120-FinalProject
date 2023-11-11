3af0803329a8fa3432f720454611b80e958c4120
Ethan Hoang <eh82059@student.sccollege.edu>
2023-11-10 18:22:46 -0800

    uploaded project

---
+++ guessTheNumber/Main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	int choice;
+
+	cout << "*** C++ Primer ***\n";
+
+	do
+	{
+		cout << "\n*** MAIN MENU ***\n";
+		cout << "\nPlease select one of the following:\n\n";
+		cout << "    1: Display my available credit\n";
+		cout << "    2: Add credits to my account\n";
+		cout << "    3: Play The Guessing game\n";
+		cout << "    4: Display My Statistics\n";
+		cout << "    5: Save My Statistics\n";
+		cout << "    6: To exit\n";
+		cout << "\nEnter your choice: ";
+		cin >> choice;
+
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore();
+			cout << "Invalid choice. Please try again and enter a valid option this time.\n";
+		}
+		else
+		{
+			switch (choice)
+			{
+			case 1:
+				cout << "You Entered 1: Display my available credit\n";
+				break;
+			case 2:
+				cout << "You Entered 2: Add credits to my account\n";
+				break;
+			case 3:
+				cout << "You Entered 3: Play The Guessing game\n";
+				break;
+			case 4:
+				cout << "You Entered 4: Display My Statistics\n";
+				break;
+			case 5:
+				cout << "You Entered 5: Save My Statistics\n";
+				break;
+			case 6:
+				cout << "Thank you for using C++ Primer software. Good bye!\n";
+				break;
+			default:
+				cout << "Invalid choice. Please try again and enter a valid option this time.\n";
+			}
+		}
+	} while (choice != 6);
+
+	return 0;
+}
